ba52c2b90847d5d0e868560e2cae2bedb8126492
Abdurrahman Sani <abdurrahmansank@gmail.com>
2026-03-29 23:58:50 +0600

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution{
+    public:
+    int  freq1[26]={0};
+    int  freq2[26]={0};
+
+    bool isAnagram(string &s,string &t){
+        for(char &c:s){
+            int index= c -'a';
+            freq1[index]++;
+        }
+        for(char &x:t){
+            int index=x-'a';
+            freq2[index]++;
+        }
+        for(int i=0;i<26;i++){
+            if(freq1[i]!=freq2[i]){
+                return false;
+            }
+        }
+        
+        
+        return true;
+    }
+};
