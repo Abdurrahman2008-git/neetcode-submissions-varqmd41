8aee50e3c34f4fe45f10502801576661a1c4f672
Abdurrahman Sani <abdurrahmansank@gmail.com>
2026-04-26 23:30:50 +0600

    Add: sliding-window-maximum - submission-1

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> res;
+    // int n=nums.size();
+    
+    for(int i=0;i<=nums.size()-k;i++){
+        int maxi=nums[i];
+        for(int j=i;j<i+k;j++){
+            maxi=max(maxi,nums[j]);
+        }
+        res.push_back(maxi);
+    }
+    return res;
+    }
+};
