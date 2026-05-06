243020c22d5a05d00f44b7de39a9c8527a275f86
Abdurrahman Sani <abdurrahmansank@gmail.com>
2026-05-06 23:33:36 +0600

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+ vector<int> dailyTemperatures(vector<int>& temperatures) {
+    
+    int n=temperatures.size();
+    vector<int> result(n);
+
+    for(int i=0;i<n;i++){
+
+        int count=1;
+        int j=i+1;
+
+        while (j<n)
+        {
+            if(temperatures[j]>temperatures[i]) break;
+            j++;
+            count++;
+        }
+        count=(j==n)? 0:count;
+        result[i]=count;
+        
+    }
+    
+    return result;
+}
+
+};
